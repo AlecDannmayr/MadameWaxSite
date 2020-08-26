f18deb021e8b0d6f54306db82eec6aba5d5c865c
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-26 19:27:00 +0100

    PHP from WP added

---
+++ MemberShipPageTheme.php
@@ -0,0 +1,144 @@
+<div id="pl-2668" class="panel-layout">
+<div id="pg-2668-0" class="panel-grid panel-has-style" data-style="{&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;,&quot;row_stretch&quot;:&quot;full&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" data-ratio="1" data-ratio-direction="right">
+<div class="siteorigin-panels-stretch panel-row-style panel-row-style-for-2668-0" data-stretch-type="full">
+<div id="pgc-2668-0-0" class="panel-grid-cell" data-weight="1">
+<div id="panel-2668-0-0-0" class="so-panel widget widget_sow-slider panel-first-child widgetopts-SO" data-index="0" data-style="{&quot;class&quot;:&quot;homeslider&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;,&quot;margin&quot;:&quot;0px 0px 0px 0px&quot;}">
+<div class="homeslider panel-widget-style panel-widget-style-for-2668-0-0-0">[siteorigin_widget class="SiteOrigin_Widget_Slider_Widget"]<input type="hidden" value="{&quot;instance&quot;:{&quot;frames&quot;:[{&quot;background_image&quot;:1705,&quot;background_image_fallback&quot;:&quot;&quot;,&quot;background_color&quot;:false,&quot;background_image_type&quot;:&quot;cover&quot;,&quot;foreground_image&quot;:0,&quot;foreground_image_fallback&quot;:&quot;&quot;,&quot;url&quot;:&quot;&quot;,&quot;background_videos&quot;:[],&quot;new_window&quot;:false},{&quot;background_image&quot;:2112,&quot;background_image_fallback&quot;:&quot;&quot;,&quot;background_color&quot;:false,&quot;background_image_type&quot;:&quot;cover&quot;,&quot;foreground_image&quot;:0,&quot;foreground_image_fallback&quot;:&quot;&quot;,&quot;url&quot;:&quot;\/brazilian-waxing&quot;,&quot;background_videos&quot;:[],&quot;new_window&quot;:false},{&quot;background_image&quot;:2113,&quot;background_image_fallback&quot;:&quot;&quot;,&quot;background_color&quot;:false,&quot;background_image_type&quot;:&quot;cover&quot;,&quot;foreground_image&quot;:0,&quot;foreground_image_fallback&quot;:&quot;&quot;,&quot;url&quot;:&quot;\/brow-bar&quot;,&quot;background_videos&quot;:[],&quot;new_window&quot;:false}],&quot;controls&quot;:{&quot;speed&quot;:600,&quot;timeout&quot;:6000,&quot;nav_color_hex&quot;:&quot;#FFFFFF&quot;,&quot;nav_style&quot;:&quot;thin&quot;,&quot;nav_size&quot;:-14,&quot;swipe&quot;:true,&quot;so_field_container_state&quot;:&quot;open&quot;},&quot;_sow_form_id&quot;:&quot;11745032335bbf4f009c489393199585&quot;,&quot;_sow_form_timestamp&quot;:&quot;1539264685814&quot;,&quot;title_link&quot;:&quot;&quot;,&quot;siteorigin&quot;:&quot;1&quot;,&quot;extended_widget_opts&quot;:{&quot;id_base&quot;:&quot;c71&quot;,&quot;visibility&quot;:{&quot;main&quot;:&quot;&quot;,&quot;options&quot;:&quot;hide&quot;,&quot;selected&quot;:&quot;0&quot;},&quot;devices&quot;:{&quot;options&quot;:&quot;hide&quot;},&quot;alignment&quot;:{&quot;desktop&quot;:&quot;default&quot;},&quot;class&quot;:{&quot;selected&quot;:&quot;0&quot;,&quot;id&quot;:&quot;&quot;,&quot;classes&quot;:&quot;&quot;,&quot;logic&quot;:&quot;&quot;}},&quot;classes&quot;:&quot;&quot;,&quot;title_link_target_blank&quot;:0,&quot;title_link_wrap&quot;:0},&quot;args&quot;:{&quot;before_widget&quot;:&quot;&lt;div id=\&quot;panel-2668-0-0-0\&quot; class=\&quot;so-panel widget widget_sow-slider panel-first-child widgetopts-SO\&quot; data-index=\&quot;0\&quot; data-style=\&quot;{&amp;quot;class&amp;quot;:&amp;quot;homeslider&amp;quot;,&amp;quot;background_image_attachment&amp;quot;:false,&amp;quot;background_display&amp;quot;:&amp;quot;tile&amp;quot;,&amp;quot;margin&amp;quot;:&amp;quot;0px 0px 0px 0px&amp;quot;}\&quot; &gt;&lt;div class=\&quot;homeslider panel-widget-style panel-widget-style-for-2668-0-0-0\&quot; &gt;&quot;,&quot;after_widget&quot;:&quot;&lt;\/div&gt;&lt;\/div&gt;&quot;,&quot;before_title&quot;:&quot;&lt;h4 class=\&quot;widget-title\&quot;&gt;&quot;,&quot;after_title&quot;:&quot;&lt;\/h4&gt;&quot;,&quot;widget_id&quot;:&quot;widget-0-0-0&quot;}}" />[/siteorigin_widget]</div>
+</div>
+<div id="panel-2668-0-0-1" class="so-panel widget widget_sow-editor panel-last-child widgetopts-SO" data-index="1" data-style="{&quot;class&quot;:&quot;flashbanner&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}">
+<div class="flashbanner panel-widget-style panel-widget-style-for-2668-0-0-1">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+
+<b>10% STUDENT DISCOUNT</b> <br class="show-for-small-only" />Monday-Friday <em>10am-4pm appointments only (min spend £30)</em>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="pg-2668-1" class="panel-grid panel-has-style" data-style="{&quot;class&quot;:&quot;homecontent&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" data-ratio="0.5" data-ratio-direction="right">
+<div class="homecontent panel-row-style panel-row-style-for-2668-1">
+<div id="pgc-2668-1-0" class="panel-grid-cell" data-weight="0.666666666667">
+<div id="panel-2668-1-0-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child widgetopts-SO" data-index="2" data-style="{&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+<h1 class="entry-title"><em>Get gorgeous. <br class="show-for-medium-down" />Get "ooh la la".</em>
+Get <em>the best</em> Brazilian wax in Cardiff.</h1>
+If you want an on-trend, precision hot wax that leaves no hair standing, you need to get Madame Waxed.
+
+Our reputation for <strong>meticulous detail</strong>, <strong>scrupulous hygiene</strong> and a <strong>razor sharp finish</strong> means smart Cardiff customers rarely trust anywhere else.
+
+Our 30-minute treatment time won't leave you feeling rushed – and it's plenty of time to ensure every last rogue hair is long gone. Plus, our superior quality hot wax and exceptional skill means becoming hair free is pain-free. You get all the <em>"Ooh"</em> without the "Ouch".
+
+You can get a <strong><a href="/top-to-toe-waxing">head to toe wax treatment</a></strong>, but the house speciality is <strong><a href="/brazilian-waxing">the Brazilian</a></strong>. It's THE best in Cardiff and will leave your skin hair free, silky soft and satin smooth – and you'll float out of your treatment oozing with confidence.
+
+We also offer a range of male waxing treatments, taking you from zero to <em>Va Va Voooom</em> in no time at all. Head over to our <strong><a title="Male Waxing" href="http://madamewax.com/male-waxing/">Monsieur Wax menu</a></strong>.
+
+So if you need to sort out down below, trust Madame Wax to leave you feeling amazing.
+
+To get started, check out our Waxing Menus and <a title="How to get Madame Waxed" href="http://madamewax.com/find-us/">opening times</a>, then call us on <strong>(029) 2023 2560 </strong>or <strong><a href="/book-online">book online</a></strong>.
+
+Your body will thank you for it. "Ooh la la!"
+
+</div>
+</div>
+</div>
+</div>
+<div id="pgc-2668-1-1" class="panel-grid-cell" data-style="{&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;,&quot;vertical_alignment&quot;:&quot;auto&quot;}" data-weight="0.333333333333">
+<div id="panel-2668-1-1-0" class="so-panel widget widget_nav_menu panel-first-child widgetopts-SO" data-index="3" data-style="{&quot;class&quot;:&quot;waxingmenu, panel&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}">
+<div class="waxingmenu panel panel-widget-style panel-widget-style-for-2668-1-1-0">[siteorigin_widget class="WP_Nav_Menu_Widget"]<input type="hidden" value="{&quot;instance&quot;:{&quot;title&quot;:&quot;Waxing Menu&quot;,&quot;nav_menu&quot;:196,&quot;extended_widget_opts&quot;:{&quot;id_base&quot;:&quot;c1664&quot;,&quot;visibility&quot;:{&quot;main&quot;:&quot;&quot;,&quot;options&quot;:&quot;hide&quot;,&quot;selected&quot;:&quot;0&quot;},&quot;devices&quot;:{&quot;options&quot;:&quot;hide&quot;},&quot;alignment&quot;:{&quot;desktop&quot;:&quot;default&quot;},&quot;class&quot;:{&quot;selected&quot;:&quot;0&quot;,&quot;id&quot;:&quot;&quot;,&quot;classes&quot;:&quot;&quot;,&quot;logic&quot;:&quot;&quot;}},&quot;title_link&quot;:&quot;&quot;,&quot;title_link_target_blank&quot;:0,&quot;title_link_wrap&quot;:0,&quot;classes&quot;:&quot;&quot;},&quot;args&quot;:{&quot;before_widget&quot;:&quot;&lt;div id=\&quot;panel-2668-1-1-0\&quot; class=\&quot;so-panel widget widget_nav_menu panel-first-child widgetopts-SO\&quot; data-index=\&quot;3\&quot; data-style=\&quot;{&amp;quot;class&amp;quot;:&amp;quot;waxingmenu, panel&amp;quot;,&amp;quot;background_image_attachment&amp;quot;:false,&amp;quot;background_display&amp;quot;:&amp;quot;tile&amp;quot;}\&quot; &gt;&lt;div class=\&quot;waxingmenu panel panel-widget-style panel-widget-style-for-2668-1-1-0\&quot; &gt;&quot;,&quot;after_widget&quot;:&quot;&lt;\/div&gt;&lt;\/div&gt;&quot;,&quot;before_title&quot;:&quot;&lt;h4 class=\&quot;widget-title\&quot;&gt;&quot;,&quot;after_title&quot;:&quot;&lt;\/h4&gt;&quot;,&quot;widget_id&quot;:&quot;widget-1-1-0&quot;}}" />[/siteorigin_widget]</div>
+</div>
+<div id="panel-2668-1-1-1" class="so-panel widget widget_text panel-last-child widgetopts-SO" data-index="4" data-style="{&quot;class&quot;:&quot;getwaxed, panel&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}">
+<div class="getwaxed panel panel-widget-style panel-widget-style-for-2668-1-1-1">
+<h4 class="widget-title">Get Madame Waxed</h4>
+<div class="textwidget">
+<div id="wpcf7-f1003-o1" class="wpcf7" dir="ltr" lang="en-US" role="form">
+<div class="screen-reader-response"></div>
+<form class="wpcf7-form" action="/wp-admin/post.php#wpcf7-f1003-o1" method="post" novalidate="novalidate">
+<div style="display: none;"><input name="_wpcf7" type="hidden" value="1003" />
+<input name="_wpcf7_version" type="hidden" value="5.0.4" />
+<input name="_wpcf7_locale" type="hidden" value="en_US" />
+<input name="_wpcf7_unit_tag" type="hidden" value="wpcf7-f1003-o1" />
+<input name="_wpcf7_container_post" type="hidden" value="0" /></div>
+<span class="wpcf7-form-control-wrap YourName"><input class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" name="YourName" size="40" type="text" value="" placeholder="Your Name*" aria-required="true" aria-invalid="false" /></span>
+
+<span class="wpcf7-form-control-wrap PhoneNumber"><input class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" name="PhoneNumber" size="40" type="tel" value="" placeholder="Phone Number*" aria-required="true" aria-invalid="false" /></span>
+
+<span class="wpcf7-form-control-wrap YourEmail"><input class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" name="YourEmail" size="40" type="email" value="" placeholder="Your Email*" aria-required="true" aria-invalid="false" /></span>
+
+<span class="wpcf7-form-control-wrap YourMessage"><textarea class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" cols="40" name="YourMessage" rows="10" placeholder="Your Message*" aria-required="true" aria-invalid="false"></textarea></span>
+
+<span id="hp5bbfc204b5ac5" class="wpcf7-form-control-wrap subject-529-wrap" style="display: none !important; visibility: hidden !important;"><label class="hp-message">Please leave this field empty.</label><input class="wpcf7-form-control wpcf7-text" tabindex="-1" autocomplete="nope" name="subject-529" size="40" type="text" value="" /></span>
+
+<input id="contact-form-button" class="wpcf7-form-control wpcf7-submit button round large expand" type="submit" value="Send" />
+
+<noscript>
+<div class="wpcf7-response-output wpcf7-spam-blocked">Your browser does not support JavaScript!. Please enable javascript in your browser in order to get form work properly.</div>
+</noscript><script type='text/javascript'>
+
+						if(contactform === undefined){
+							var contactform = [];
+						}
+						var innerVal = [1003,'mail_sent_ok','Your message was sent successfully. Thanks.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'mail_sent_ng','Failed to send your message. Please try later or contact the administrator by another method.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'validation_error','Validation errors occurred. Please confirm the fields and submit it again.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'spam','Failed to send your message. Please try later or contact the administrator by another method.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'accept_terms','Please accept the terms to proceed.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_required','Please fill the required field.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_too_long','The field is too long.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_too_short','The field is too short.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_date','Date format seems invalid.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'date_too_early','This date is too early.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'date_too_late','This date is too late.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'upload_failed','Failed to upload file.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'upload_file_type_invalid','This file type is not allowed.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'upload_file_too_large','This file is too large.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'upload_failed_php_error','Failed to upload file. Error occurred.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_number','Number format seems invalid.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'number_too_small','This number is too small.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'number_too_large','This number is too large.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'quiz_answer_not_correct','Your answer is not correct.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'captcha_not_match','Your entered code is incorrect.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_email','Email address seems invalid.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_url','URL seems invalid.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'invalid_tel','Telephone number seems invalid.'];
+						contactform.push(innerVal);
+						var innerVal = [1003,'gdpr',''];
+						contactform.push(innerVal);
+						</script>
+<div class="wpcf7-response-output wpcf7-display-none"></div>
+</form></div>
+<a class="button round large book-online" style="display: block; margin-bottom: .8rem;" href="http://madamewax.com/book-online">Book Online</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
