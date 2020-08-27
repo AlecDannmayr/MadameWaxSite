e2d9dec502c92478794bb8f979230f0351a077fb
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-27 22:35:26 +0100

    PHP WordPress Update

--- MembershipPage.php
+++ MembershipPage.php
@@ -11,9 +11,76 @@ get_header(); ?>
 			while ( have_posts() ) :
 				the_post();
 				?>
-				
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
 			<?php endwhile; // end of the loop. ?>
+
+
+
+            <div id="pg-1904-2" class="panel-grid panel-no-style" data-style="{&quot;background_display&quot;:&quot;tile&quot;,&quot;bottom_margin&quot;:&quot;60px&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}">
+<div id="pgc-1904-2-0" class="panel-grid-cell" data-weight="0.333333333333">
+<div id="panel-1904-2-0-0" class="so-panel widget widget_sow-editor panel-first-child widgetopts-SO" data-index="4" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1989&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}">
+<div class="circle deal panel-widget-style panel-widget-style-for-1904-2-0-0">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+<h2><strong>Opal</strong></h2>
+Hollywood &amp; Bespoke Brow
+
+from <span style="font-size: 140%;"><strong>£73</strong></span>
+
+</div>
+</div>
+</div>
+</div>
+<div id="panel-1904-2-0-1" class="so-panel widget widget_sow-editor panel-last-child widgetopts-SO" data-index="5" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1990&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}">
+<div class="circle deal panel-widget-style panel-widget-style-for-1904-2-0-1">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+<h2><strong>Emerald</strong></h2>
+Full Leg and Lola wax
+
+from <span style="font-size: 140%;"><strong>£54</strong></span>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="pgc-1904-2-1" class="panel-grid-cell" data-weight="0.333333333333">
+<div id="panel-1904-2-1-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child widgetopts-SO" data-index="6" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1990&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}">
+<div class="circle deal panel-widget-style panel-widget-style-for-1904-2-1-0">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+<h2><strong>Sapphire</strong></h2>
+Full Wax Package. Full Leg, Hollywood/Brazilian &amp; Underarm
+
+from <span style="font-size: 140%;"><strong>£87</strong></span>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="pgc-1904-2-2" class="panel-grid-cell" data-weight="0.333333333333">
+<div id="panel-1904-2-2-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child widgetopts-SO" data-index="7" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1991&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}">
+<div class="circle deal panel-widget-style panel-widget-style-for-1904-2-2-0">
+<div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+<h2><strong>Ruby</strong></h2>
+A course of 5 Bespoke Brows. £25 each instead of £35 when you buy this no-brainer offer, saving you £50. Valid for 6 months from purchase.
+
+from <span style="font-size: 140%;"><strong>£125</strong></span>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
