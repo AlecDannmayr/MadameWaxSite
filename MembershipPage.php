90a30c4cf250f8cb254fb74a2772f1586bd07e54
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-27 21:59:53 +0100

    WordPress PHP Template, disabled comments etc

--- MembershipPage.php
+++ MembershipPage.php
@@ -11,8 +11,7 @@ get_header(); ?>
 			while ( have_posts() ) :
 				the_post();
 				?>
-				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
+				
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
