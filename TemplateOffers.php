da573b0392f77199a58b568baf34ebb7c429d7bf
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-27 18:39:01 +0100

    php file added

--- TemplateOffers.php
+++ TemplateOffers.php
@@ -1,16 +1,6 @@
-<? /* Template Name: MemberShipOffersPage */ ?> 
 
-<div class="siteorigin-widget-tinymce textwidget">
-	<h3><strong>20% off for besties</strong></h3>
-We <em>love</em> rewarding clients who spread the word about Madame Wax.
+<div id="pl-3451"  class="panel-layout" ><div id="pg-3451-0"  class="panel-grid panel-no-style"  data-style="{&quot;background_display&quot;:&quot;cover&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" ><div id="pgc-3451-0-0"  class="panel-grid-cell"  data-weight="1" ><div id="panel-3451-0-0-0" class="so-panel widget widget_sow-editor panel-first-child" data-index="0" data-style="{&quot;background_display&quot;:&quot;tile&quot;}" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
 
-<strong>If you refer a friend (who spends at least £30 on wax treatments), you’ll BOTH get a 20% discount on your 2nd visit to us. </strong></div>
-</div></div></div><div id="panel-3451-0-0-2" class="so-panel widget widget_sow-editor panel-last-child widgetopts-SO" data-index="2" data-style="{&quot;class&quot;:&quot;rounded purplegrad&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background&quot;:&quot;#ffffff&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}" ><div class="rounded purplegrad panel-widget-style panel-widget-style-for-3451-0-0-2" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
-<div class="siteorigin-widget-tinymce textwidget">
-	<h3><strong>10% off for students</strong></h3>
-<p>Students LOVE Madame Wax – and we return the love with a 10% discount.</p>
-<p>Available Monday-Friday between 10am and 4pm. (min spend £30) Simply pre-book your appointment, show us your NUS card when you come in for treatment, and save yourself a few ££££s.</p>
-</div>
 </div></div></div></div></div><div id="pg-3451-1"  class="panel-grid panel-no-style"  data-style="{&quot;background_display&quot;:&quot;cover&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" ><div id="pgc-3451-1-0"  class="panel-grid-cell"  data-weight="1" ><div id="panel-3451-1-0-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child" data-index="3" data-style="{&quot;background_display&quot;:&quot;tile&quot;}" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
 <div class="siteorigin-widget-tinymce textwidget">
 	<h1>Save more with a Madame Wax Package</h1><p>Some treatments are meant to be enjoyed together. Invest in one of our packages not only will you save £££s, but you’ll leave with extra <em>Ooh la la!</em></p></div>
