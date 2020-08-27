27f8e9c4de8c232fefd38a2e6626770634ad81ec
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-08-27 18:35:14 +0100

    PHP file added

---
+++ TemplateOffers.php
@@ -0,0 +1,43 @@
+<div id="pl-3451"  class="panel-layout" ><div id="pg-3451-0"  class="panel-grid panel-no-style"  data-style="{&quot;background_display&quot;:&quot;cover&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" ><div id="pgc-3451-0-0"  class="panel-grid-cell"  data-weight="1" ><div id="panel-3451-0-0-0" class="so-panel widget widget_sow-editor panel-first-child" data-index="0" data-style="{&quot;background_display&quot;:&quot;tile&quot;}" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<p>We know you LOVE a good deal! That's why we've put together a collection of special offers for our lovely customers (including you).</p><p>So if you want to save £££s on your favourite wax and brow treatments, here's what we've got for you. <em>Ooh la la!</em><br /> <strong>P.S.</strong> Whilst your favourites are here to stay, we do shake up our deals from time to time. So remember to check back in at a later date to see what else we've got for you.</p></div>
+</div></div><div id="panel-3451-0-0-1" class="so-panel widget widget_sow-editor widgetopts-SO" data-index="1" data-style="{&quot;class&quot;:&quot;rounded purplegrad&quot;,&quot;widget_css&quot;:&quot;color:white;&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background&quot;:&quot;#ffffff&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}" ><div class="rounded purplegrad panel-widget-style panel-widget-style-for-3451-0-0-1" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h3><strong>20% off for besties</strong></h3>
+We <em>love</em> rewarding clients who spread the word about Madame Wax.
+
+<strong>If you refer a friend (who spends at least £30 on wax treatments), you’ll BOTH get a 20% discount on your 2nd visit to us. </strong></div>
+</div></div></div><div id="panel-3451-0-0-2" class="so-panel widget widget_sow-editor panel-last-child widgetopts-SO" data-index="2" data-style="{&quot;class&quot;:&quot;rounded purplegrad&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background&quot;:&quot;#ffffff&quot;,&quot;background_image_attachment&quot;:false,&quot;background_display&quot;:&quot;tile&quot;}" ><div class="rounded purplegrad panel-widget-style panel-widget-style-for-3451-0-0-2" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h3><strong>10% off for students</strong></h3>
+<p>Students LOVE Madame Wax – and we return the love with a 10% discount.</p>
+<p>Available Monday-Friday between 10am and 4pm. (min spend £30) Simply pre-book your appointment, show us your NUS card when you come in for treatment, and save yourself a few ££££s.</p>
+</div>
+</div></div></div></div></div><div id="pg-3451-1"  class="panel-grid panel-no-style"  data-style="{&quot;background_display&quot;:&quot;cover&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" ><div id="pgc-3451-1-0"  class="panel-grid-cell"  data-weight="1" ><div id="panel-3451-1-0-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child" data-index="3" data-style="{&quot;background_display&quot;:&quot;tile&quot;}" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h1>Save more with a Madame Wax Package</h1><p>Some treatments are meant to be enjoyed together. Invest in one of our packages not only will you save £££s, but you’ll leave with extra <em>Ooh la la!</em></p></div>
+</div></div></div></div><div id="pg-3451-2"  class="panel-grid panel-no-style"  data-style="{&quot;background_display&quot;:&quot;tile&quot;,&quot;bottom_margin&quot;:&quot;60px&quot;,&quot;cell_alignment&quot;:&quot;flex-start&quot;}" ><div id="pgc-3451-2-0"  class="panel-grid-cell"  data-weight="0.333333333333" ><div id="panel-3451-2-0-0" class="so-panel widget widget_sow-editor panel-first-child widgetopts-SO" data-index="4" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1989&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}" ><div class="circle deal panel-widget-style panel-widget-style-for-3451-2-0-0" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h2><strong>Opal</strong></h2>
+<p>Hollywood &amp; Bespoke Brow</p>
+<p>from <span style="font-size: 140%;"><strong>£73</strong></span></p>
+</div>
+</div></div></div><div id="panel-3451-2-0-1" class="so-panel widget widget_sow-editor panel-last-child widgetopts-SO" data-index="5" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1990&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}" ><div class="circle deal panel-widget-style panel-widget-style-for-3451-2-0-1" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h2><strong>Emerald</strong></h2>
+<p>Full Leg and Lola wax</p>
+<p>from <span style="font-size: 140%;"><strong>£54</strong></span></p>
+</div>
+</div></div></div></div><div id="pgc-3451-2-1"  class="panel-grid-cell"  data-weight="0.333333333333" ><div id="panel-3451-2-1-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child widgetopts-SO" data-index="6" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1990&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}" ><div class="circle deal panel-widget-style panel-widget-style-for-3451-2-1-0" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h2><strong>Sapphire</strong></h2>
+<p>Full Wax Package. Full Leg, Hollywood/Brazilian &amp; Underarm</p>
+<p>from <span style="font-size: 140%;"><strong>£87</strong></span></p>
+</div>
+</div></div></div></div><div id="pgc-3451-2-2"  class="panel-grid-cell"  data-weight="0.333333333333" ><div id="panel-3451-2-2-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child widgetopts-SO" data-index="7" data-style="{&quot;class&quot;:&quot;circle deal&quot;,&quot;padding&quot;:&quot;30px 30px 30px 30px&quot;,&quot;background_image_attachment&quot;:&quot;1991&quot;,&quot;background_image_attachment_fallback&quot;:false,&quot;background_display&quot;:&quot;cover&quot;}" ><div class="circle deal panel-widget-style panel-widget-style-for-3451-2-2-0" ><div class="so-widget-sow-editor so-widget-sow-editor-base">
+<div class="siteorigin-widget-tinymce textwidget">
+	<h2><strong>Ruby</strong></h2>
+<p>A course of 5 Bespoke Brows. £25 each instead of £35 when you buy this no-brainer offer, saving you £50. Valid for 6 months from purchase.</p>
+<p>from <span style="font-size: 140%;"><strong>£125</strong></span></p>
+</div>
+</div></div></div></div></div></div>
